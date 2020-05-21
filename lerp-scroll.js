082c6a82f94f8a3d6d9d5f3729e0ab82063c04a0
Marcin Kuna <marcin.m.kuna@gmail.com>
2020-05-21 22:39:48 +0200

    Lerp scroll - WIP

---
+++ lerp-scroll.js
@@ -0,0 +1,39 @@
+const body = document.body;
+const main = document.querySelector('main');
+
+let sx = 0;
+let sy = 0;
+let dx = sx;
+let dy = sy;
+
+body.style.height = main.clientHeight + 'px';
+
+main.style.position = 'fixed';
+main.style.top = 0;
+main.style.left = 0;
+
+window.addEventListener('scroll', scroll);
+
+function scroll() {
+    // sx = window.pageXOffset; //oryginał
+    // sy = window.pageYOffset; //oryginał
+    sx = window.pageXOffset*.4;
+    sy = window.pageYOffset*.4;
+}
+
+window.requestAnimationFrame(render);
+
+function render() {
+    dx = lerp(dx, sx, 0.07);
+    dy = lerp(dy, sy, 0.07);
+    dx = Math.floor(dx * 100) / 100;
+    dy = Math.floor(dy * 100) / 100;
+
+    main.style.transform = `translate(-${dx}px, -${dy}px)`;
+
+    window.requestAnimationFrame(render);
+}
+
+function lerp(a, b, n) {
+    return (1 - n) * a + n * b;
+}
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// MENU & OVERLAY
+
 const bgChange = document.querySelectorAll('.bg-change')
 const menuBtn = document.querySelector('.menu-btn')
 const navLinks = document.querySelectorAll('.nav-link')
@@ -26,3 +28,58 @@ menuBtn.addEventListener('click', () => {
     menuOpen = !menuOpen
 })
 
+// SMOOTH SCROLL
+
+navLinks.forEach(elem => elem.addEventListener('click', navLinkClick));
+
+function navLinkClick(event) {
+    smoothScroll(event);
+    menuBtn.classList.toggle('menu-open')
+
+    navLinks.forEach(element => {
+        element.classList.toggle('menu-open')
+    })
+
+    if(menuOpen === false) {
+        bgChange.forEach(element => {
+            element.classList.add('slide')
+        })
+    } else {
+        setTimeout(() => {
+            bgChange.forEach(element => {
+                element.classList.remove('slide')
+            })
+        }, 1000)
+    }
+
+    menuOpen = !menuOpen
+}
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href');
+    const targetPosition = document.querySelector(targetId).offsetTop; 
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 800;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+      if(!start) start = timestamp;
+      const progress = timestamp - start;
+    //   window.scrollTo(0, distance*(progress/duration) + startPosition);    //linear
+      window.scrollTo(0, easeInOutQuad(progress, startPosition, distance, duration));
+      if(progress < duration) window.requestAnimationFrame(step);
+    }
+}
+
+// EASING FOR SMOOTH SCROLL
+
+function easeInOutQuad(t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+};
