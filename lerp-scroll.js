cfc5305fa6f1d00f15c5b04d5127846906ff5a8e
Marcin Kuna <marcin.m.kuna@gmail.com>
2020-07-13 18:37:40 +0200

    Media queries - Footer

--- lerp-scroll.js
+++ lerp-scroll.js
@@ -1,39 +1,45 @@
-const body = document.body;
-const main = document.querySelector('main');
-
-
-let sx = 0;
-let sy = 0;
-let dx = sx;
-let dy = sy;
-
-body.style.height = main.clientHeight + 'px';
-
-main.style.position = 'fixed';
-main.style.top = 0;
-main.style.left = 0;
-
-window.addEventListener('scroll', scroll);
-
-function scroll() {
-    sx = window.pageXOffset;
-    sy = window.pageYOffset;
-}
-
-window.requestAnimationFrame(render);
-
-function render() {
-    dx = lerp(dx, sx, 0.07);
-    dy = lerp(dy, sy, 0.07);
-    dx = Math.floor(dx * 100) / 100;
-    dy = Math.floor(dy * 100) / 100;
-
-    main.style.transform = `translate(-${dx}px, -${dy}px)`;
-
+const lerpScroll = () => {
+    const body = document.body;
+    const main = document.querySelector('main');
+    
+    let sx = 0;
+    let sy = 0;
+    let dx = sx;
+    let dy = sy;
+    
+    body.style.height = main.clientHeight + 'px';
+    
+    main.style.position = 'fixed';
+    main.style.top = 0;
+    main.style.left = 0;
+    
+    window.addEventListener('scroll', scroll);
+    
+    function scroll() {
+        sx = window.pageXOffset;
+        sy = window.pageYOffset;
+    }
+    
     window.requestAnimationFrame(render);
+    
+    function render() {
+        dx = lerp(dx, sx, 0.07);
+        dy = lerp(dy, sy, 0.07);
+        dx = Math.floor(dx * 100) / 100;
+        dy = Math.floor(dy * 100) / 100;
+    
+        main.style.transform = `translate(-${dx}px, -${dy}px)`;
+    
+        window.requestAnimationFrame(render);
+    }
+    
+    function lerp(a, b, n) {
+        return (1 - n) * a + n * b;
+    }
 }
 
-function lerp(a, b, n) {
-    return (1 - n) * a + n * b;
-}
+lerpScroll()
+
+// Adjustable height on orientation/screen size change (prevent fixed height whitespace 'bug')
+window.onresize = lerpScroll
 
